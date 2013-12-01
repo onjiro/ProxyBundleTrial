0495f742cd6c346485eb107897490b5c69f8911d
onjiro <onjiro.mohyahya@gmail.com>
2013-12-01 14:44:28 +0000

    Create sample for generate method

--- src/Onjiro/Bundle/MemcacheProxyBundle/Controller/DefaultController.php
+++ src/Onjiro/Bundle/MemcacheProxyBundle/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Zend\Code\Generator\ClassGenerator;
+use Zend\Code\Generator\MethodGenerator;
+use Zend\Code\Generator\ParameterGenerator;
 
 class DefaultController extends Controller
 {
@@ -16,9 +18,28 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        $method = new MethodGenerator(
+            $name = 'method',
+            $parameters = [
+                new ParameterGenerator('parameters', 'array', [])
+            ],
+            $flags = MethodGenerator::FLAG_PUBLIC,
+            $body = "echo 'Generated method called!!';",
+            null
+        );
+
         $class = new ClassGenerator();
         $class->setName('GenerateExample');
+        $class->addMethods([
+            $method,
+        ]);
         $code = $class->generate();
+
+        echo $code."\n\n";
+
+        eval($code);
+        $generatedClass = new \GenerateExample();
+        echo $generatedClass->method()."\n\n";
         
         return new JsonResponse(["generated class" => $code]);
     }
